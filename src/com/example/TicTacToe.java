3904692293fd335002d16693f2d81d3434ad485a
krove <krove@DESKTOP-Q5SED1R>
2020-10-08 03:11:46 +0530

    [Kashif]Added:Method to Allow User to Choose A Letter

--- src/com/example/TicTacToe.java
+++ src/com/example/TicTacToe.java
@@ -1,14 +1,34 @@
 package com.example;
 
+import java.util.*;
+
 public class TicTacToe {
-	public void addBoard() {
+	Scanner sc = new Scanner(System.in);
+	public char[] addBoard() {
 		char [] board = new char[10];
 		for(int i=1; i<10; i++)
 			board[i] = ' ';
+		return board;
+	}
+	public char takeInput(){	
+		System.out.println("Please choose either X or O:");
+		char user = sc.next().charAt(0);
+		return user;
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome");
-		TicTacToe t = new TicTacToe();
+		char comp = ' ';
+		TicTacToe t= new TicTacToe();
 		t.addBoard();
+		char user = t.takeInput();
+		if(user == 'X') {
+			comp = 'O';}
+		else if(user =='O') {
+			comp='X';}
+		else
+		{
+			System.out.println("Wrong choice");
+		}
+		System.out.println("User-"+user+" Computer-"+comp);	
 	}
 }

6dd3d8955f56bad89daec8c9f0f92cb41f862b50
krove <krove@DESKTOP-Q5SED1R>
2020-10-08 03:13:49 +0530

    [Kashif]Added:Method to Display Board

--- src/com/example/TicTacToe.java
+++ src/com/example/TicTacToe.java
@@ -6,7 +6,7 @@ public class TicTacToe {
 	Scanner sc = new Scanner(System.in);
 	public char[] addBoard() {
 		char [] board = new char[10];
-		for(int i=1; i<10; i++)
+		for(int i=1; i<board.length; i++)
 			board[i] = ' ';
 		return board;
 	}
@@ -15,11 +15,29 @@ public class TicTacToe {
 		char user = sc.next().charAt(0);
 		return user;
 	}
+	public void showBoard(char[] board) {
+		int i, j = 0;
+		int boardPosition = 1;
+		char[][] gameBoard = new char[3][3];
+		System.out.println("Displaying Board :");
+		for (i = 0; i < 3; i++) {
+			for (j = 0; j < 3; j++) {
+				gameBoard[i][j] = board[boardPosition];
+				System.out.print(gameBoard[i][j]);
+				if (j < 2)
+					System.out.print(" | ");
+				boardPosition++;
+			}
+			System.out.println();
+			if (i < 2)
+				System.out.println("---------");
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		char comp = ' ';
 		TicTacToe t= new TicTacToe();
-		t.addBoard();
+		char [] board=t.addBoard();
 		char user = t.takeInput();
 		if(user == 'X') {
 			comp = 'O';}
@@ -29,6 +47,7 @@ public class TicTacToe {
 		{
 			System.out.println("Wrong choice");
 		}
-		System.out.println("User-"+user+" Computer-"+comp);	
+		System.out.println("User-"+user+" Computer-"+comp);
+		t.showBoard(board);
 	}
 }

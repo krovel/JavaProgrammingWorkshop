2b43f93ffdbf7cd578c17eaf1696df9099f3ece1
krove <krove@DESKTOP-Q5SED1R>
2020-10-08 03:07:34 +0530

    [Kashif]Created:Empty board

--- src/com/example/TicTacToe.java
+++ src/com/example/TicTacToe.java
@@ -1,7 +1,14 @@
 package com.example;
 
 public class TicTacToe {
-public static void main(String[] args) {
-	System.out.println("Welcome");
-}
+	public void addBoard() {
+		char [] board = new char[10];
+		for(int i=1; i<10; i++)
+			board[i] = ' ';
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		TicTacToe t = new TicTacToe();
+		t.addBoard();
+	}
 }
